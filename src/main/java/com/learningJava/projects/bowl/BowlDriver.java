0450ee54463e7de32cea298ffe01e20f4acc33e1
Falafel <tadgranite@gmail.com>
2023-10-15 15:45:03 -0500

    reorg

---
+++ src/main/java/com/learningJava/projects/bowl/BowlDriver.java
@@ -0,0 +1,8 @@
+package com.learningJava.projects.bowl;
+
+public class BowlDriver {
+    public static void main (String[] args) {
+        // TODO create a bowl object and assign it to a variable called bowl, setting it's radius, height, and material to 15, 12, and "Glass", respectively
+        // TODO print out the toString method of the bowl
+    }
+}
