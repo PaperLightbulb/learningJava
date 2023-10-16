ff65e4096ee3cf4e129cc7dc0d03189e6a828a5c
Tim Garnett <tadgranite@gmail.com>
2023-10-16 13:37:09 -0500

    made Cupboard.java

---
+++ src/main/java/com/learningJava/projects/cupboard/Cupboard.java
@@ -0,0 +1,9 @@
+package com.learningJava.projects.cupboard;
+
+public class Cupboard {
+    // TODO make and set 3 bowl variables
+    // TODO make a constructor
+    // TODO make a cool toString() method
+
+    // TODO BONUS make the bowls an array
+}
---
+++ src/main/java/com/learningJava/projects/cupboard/CupboardDriver.java
@@ -0,0 +1,7 @@
+package com.learningJava.projects.cupboard;
+
+public class CupboardDriver {
+    // TODO make a cupboard variable and set it
+    // TODO mess with the internals of the bowls by setting the size etc of the bowls in the cupboard
+    // TODO print the toString() method of the cupboard
+}
