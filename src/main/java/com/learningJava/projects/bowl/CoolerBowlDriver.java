f5ab4502b1d6836ceb0c33c9fc9566d5741354a6
Tim Garnett <tadgranite@gmail.com>
2023-10-16 13:20:58 -0500

    made cooler bowldriver

---
+++ src/main/java/com/learningJava/projects/bowl/CoolerBowlDriver.java
@@ -0,0 +1,9 @@
+package com.learningJava.projects.bowl;
+
+public class CoolerBowlDriver {
+    // TODO create a bowl object and set it to a variable called bowl
+    // TODO take input from the user and set the bowl's variables
+    // TODO print the toString() of the bowl
+
+    // TODO BONUS redo the bowl's variables to be not public and use getters and setters
+}
