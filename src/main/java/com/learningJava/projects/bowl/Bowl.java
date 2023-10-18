0bc7cfef24d3751f103b99a7cc2f98d794397c7a
Falafel <tadgranite@gmail.com>
2023-10-18 17:13:55 -0500

    merg mess up fix

--- src/main/java/com/learningJava/projects/bowl/Bowl.java
+++ src/main/java/com/learningJava/projects/bowl/Bowl.java
@@ -5,30 +5,25 @@ public class Bowl {
     // TODO Create public String variable material,
     // TODO Create a null constructor,
     // TODO Create constructor method that sets each of the variables,
-    // TODO Create a toString() method that returns the string "Radius: " + radius + ". Height: " + height + ". Material: " + material + "."
-    private int radius;
-    private int height;
+    // TODO Create a toString() method that returns the string "Radius: " + radius + ". Height: " + height + ". Material: " + material + "."\
+    private   int radius;
+    private  int height;
     private String material;
-    public Bowl(int radius,int height, String material){
-        this.radius=radius;
-        this.height=height;
-        this.material=material;
-
-    }
-    public String toString(){
-        return "Radius: " + radius + ". Height: " + height + ". Material: " + material + ".";
-    }
-
-    public void setHeight(int height) {
+    public Bowl (int radius, int height, String material) {
+        this.radius = radius;
         this.height = height;
+        this. material = material;
+    }
+    public String toString () {
+        return "Radius: " + radius + ". Height: " + height + ". Material: " + material + ".";
     }
 
     public int getHeight() {
         return height;
     }
 
-    public void setMaterial(String material) {
-        this.material = material;
+    public int getRadius() {
+        return radius;
     }
 
     public String getMaterial() {
@@ -39,7 +34,11 @@ public class Bowl {
         this.radius = radius;
     }
 
-    public int getRadius() {
-        return radius;
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
     }
 }
