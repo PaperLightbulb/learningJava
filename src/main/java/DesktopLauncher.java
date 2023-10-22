d298e70c25f7e6f8fb447383cf41757c134407da
Falafel <tadgranite@gmail.com>
2023-10-22 00:15:51 -0500

    idk man

---
+++ src/main/java/DesktopLauncher.java
@@ -0,0 +1,14 @@
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setIdleFPS(60);
+        configuration.useVsync(true);
+        configuration.setTitle("Game");
+
+        configuration.setWindowedMode(920, 640);
+        new Lwjgl3Application(new Boot(), configuration);
+    }
+}
