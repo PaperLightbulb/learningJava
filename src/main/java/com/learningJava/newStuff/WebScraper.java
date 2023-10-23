fae2317d2314e0aac64bd74891e7522b1fa9e573
Falafel <tadgranite@gmail.com>
2023-10-22 22:49:33 -0500

    forgot to delete some unused vars

--- src/main/java/com/learningJava/newStuff/WebScraper.java
+++ src/main/java/com/learningJava/newStuff/WebScraper.java
@@ -6,16 +6,8 @@ import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 
 import java.io.*;
-import java.util.regex.*;
 
 public class WebScraper {
-    private static Pattern patternDomainName;
-    private Matcher matcher;
-    private static final String DOMAIN_NAME_PATTERN
-            = "([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}";
-    static {
-        patternDomainName = Pattern.compile(DOMAIN_NAME_PATTERN);
-    }
     public static void main(String[] args) {
         String url = "https://forecast.weather.gov/MapClick.php?lat=45.1089&lon=-93.3472";
         // initializing the HTML Document page variable
